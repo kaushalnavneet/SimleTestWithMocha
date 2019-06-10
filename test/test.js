1df8d5171735eafdba40e25b4808de2ab1e43e30
David Voicu <davidvoicu@rogers.com>
2019-06-10 11:27:47 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
 var utils = require("../utils");
 
 var assert = require('assert');
-describe('TEST', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
+assert.equal(1, true);
+// describe('TEST', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1,2,3].indexOf(4), -1);
+//     });
+//   });
   
 //   describe('equal to 1', function() {
 //     it('show be 1', function() {
