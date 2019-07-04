c75cb8ef08990836187eafd59fdd244df6c14330
David Voicu <david.voicu@ibm.com>
2019-07-04 10:38:05 -0400

    updated

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #Invoke tests here
 
-export NVM_DIR=/home/pipeline/nvm
-export NODE_VERSION=6.13.1
-export NVM_VERSION=0.29.0
+# export NVM_DIR=/home/pipeline/nvm
+# export NODE_VERSION=6.13.1
+# export NVM_VERSION=0.29.0
 
-npm config delete prefix \
-  && curl https://raw.githubusercontent.com/creationix/nvm/v${NVM_VERSION}/install.sh | sh \
-  && . $NVM_DIR/nvm.sh \
-  && nvm install $NODE_VERSION \
-  && nvm alias default $NODE_VERSION \
-  && nvm use default \
-  && node -v \
-  && npm -v
+# npm config delete prefix \
+#   && curl https://raw.githubusercontent.com/creationix/nvm/v${NVM_VERSION}/install.sh | sh \
+#   && . $NVM_DIR/nvm.sh \
+#   && nvm install $NODE_VERSION \
+#   && nvm alias default $NODE_VERSION \
+#   && nvm use default \
+#   && node -v \
+#   && npm -v
 
 npm install
 npm run test
--- test/test.js
+++ test/test.js
@@ -7,24 +7,4 @@ describe('TEST', function() {
       assert.equal([1,2,3].indexOf(4), -1);
     });
   });
-  
-  describe('equal to 1', function() {
-    it('show be 1', function() {
-      assert.equal(utils.showcase(), 1);
-    });
-  });
-  
-  describe('this one fails', function() {
-    it('show passes', function() {
-      assert.equal(utils.showcase(), 1);
-    });
-    
-     it('show fail2', function() {
-      assert.equal(utils.showcase(), 2);
-    });
-    
-    it('show fail3', function() {
-      assert.equal(utils.showcase(), 3);
-    });
-  });
 });
